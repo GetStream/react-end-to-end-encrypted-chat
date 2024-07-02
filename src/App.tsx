ae0532cbc9640f6cf23e23ad91085e74865e1847
Stefan <stefan.blos@gmail.com>
2024-07-02 12:10:53 +0200

    Added more code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import type { User, ChannelSort, ChannelFilters, ChannelOptions } from 'stream-chat';
+import type {
+  User,
+  ChannelSort,
+  ChannelFilters,
+  ChannelOptions,
+} from 'stream-chat';
 import {
   useCreateChatClient,
   Chat,
@@ -10,20 +15,21 @@ import {
   Thread,
   Window,
 } from 'stream-chat-react';
+import { v4 as uuidv4 } from 'uuid';
+import { Buffer } from 'buffer';
 
-import SealdSDK from '@seald-io/sdk/browser' // if your bundler supports the "browser" field in package.json (supported by Webpack 5)
+import SealdSDK from '@seald-io/sdk/browser'; // if your bundler supports the "browser" field in package.json (supported by Webpack 5)
+import { SignJWT } from 'jose';
 
 import 'stream-chat-react/dist/css/v2/index.css';
 import './layout.css';
-
-// Seald credentials
-const appId = "your_app_id_here"; // Replace "your_app_id_here" with your actual app ID.
-const apiURL = "https://your.api.url.here";
+import { useCallback, useEffect } from 'react';
 
 const apiKey = 'dz5f4d5kzrue';
-const userId = 'little-unit-1';
-const userName = 'little';
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibGl0dGxlLXVuaXQtMSIsImV4cCI6MTcxOTMxOTc3N30.KFPpxzxQz40RP_fUA_AeustoTGadk-hFGTHEi3x75HA';
+const userId = 'royal-darkness-8';
+const userName = 'royal';
+const userToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm95YWwtZGFya25lc3MtOCIsImV4cCI6MTcxOTkzMDA1OH0.2riCvKvoWrbGtgltpN1jTerAIMxN0_gIIlytiQqSCzc';
 
 const user: User = {
   id: userId,
@@ -40,9 +46,43 @@ const options: ChannelOptions = {
   limit: 10,
 };
 
-const seald = SealdSDK({ appId, apiURL })
+// Seald credentials
+const appId = import.meta.env.VITE_SEALD_APP_ID;
+const apiURL = import.meta.env.VITE_API_URL;
+
+const seald = SealdSDK({ appId, apiURL });
 
 const App = () => {
+  const registerSeald = useCallback(async function registerSeald() {
+    const signUpJWT = await createSignupJWT();
+    console.log('signUpJWT', signUpJWT);
+    await seald.initiateIdentity({ signupJWT: signUpJWT });
+  }, []);
+
+  useEffect(() => {
+    registerSeald();
+  }, [registerSeald]);
+
+  async function createSignupJWT() {
+    'use server';
+
+    const jwtSecret = import.meta.env.VITE_JWT_SECRET;
+    const jwtSecretId = import.meta.env.VITE_JWT_SECRET_ID;
+    console.log('jwtSecret', jwtSecret);
+    console.log('jwtSecretId', jwtSecretId);
+
+    const token = new SignJWT({
+      iss: jwtSecretId,
+      jti: uuidv4(), // So the JWT is only usable once. The `random` generates a random string, with enough entropy to never repeat : a UUIDv4 would be a good choice.
+      iat: Math.floor(Date.now() / 1000), // JWT valid only for 10 minutes. `Date.now()` returns the timestamp in milliseconds, this needs it in seconds.
+      scopes: [3], // PERMISSION_JOIN_TEAM
+      join_team: true,
+    }).setProtectedHeader({ alg: 'HS256' });
+
+    const signupJWT = await token.sign(Buffer.from(jwtSecret, 'ascii'));
+    return signupJWT;
+  }
+
   const client = useCreateChatClient({
     apiKey,
     tokenOrProvider: userToken,
@@ -58,7 +98,25 @@ const App = () => {
         <Window>
           <ChannelHeader />
           <MessageList />
-          <MessageInput />
+          <MessageInput
+            overrideSubmitHandler={async (
+              message,
+              channelId,
+              customMessageData,
+              options
+            ) => {
+              console.log('message', message);
+              if (message.text) {
+                console.log('Starting message encryption');
+                // const encryptedMessage = await seald.encryptMessage(
+                //   message.text,
+                //   { sealdIds: [sealdId.sealdId] }
+                // );
+
+                // console.log('encryptedMessage', encryptedMessage);
+              }
+            }}
+          />
         </Window>
         <Thread />
       </Channel>
